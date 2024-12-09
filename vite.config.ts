332f0869f7a0ff0068008c6f1be86feb6ac8942d
Cristian Arley Pareja Londoño <capareja@siesa.com>
2024-12-08 21:14:19 -0500

    Fix config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   base:"/gh-pages-entrega3/",
-  plugins: [solidPlugin()],
-  build: {
-    target: 'esnext',
-  },
+  plugins: [solid()],
 })
